b453d4dd831187811758ad1267cd7e46fa64f5fd
wanzew <wanzew@163.com>
2018-11-15 22:35:38 +0800

    my first summit

---
+++ main/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include"template.cpp"
+
+using namespace std;
+
+int main(){
+
+    string wang = "wanzew";
+
+    printInfo(wang);
+
+    printInfo(wang);
+
+    return 0;
+}
---
+++ src/template.cpp
@@ -0,0 +1,10 @@
+// #include"template.h"
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+template <class T>
+void printInfo(T f_param){
+    cout<<"template function printInfo: "<<f_param<<endl;
+}
